94e66cb4b811bf72f06eb1ec00fcbc499ddfac98
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-14 14:35:37 +0100

    Kata persistence zaliczone

--- codewars/persistence/persistence.py
+++ codewars/persistence/persistence.py
@@ -1,7 +1,30 @@
 
-def persistence(liczba: int) -> int:
+def persistence(n: int) -> int:
+    i = 0
 
-    return 3
+    def int_to_list(m):
+        lista = []
+        for n in str(m):
+            if n.isdigit():
+                lista.append(int(n))
+        return lista
+
+    def multiplication(lista: list):
+
+        if len(lista) == 1:
+            return lista.pop(0)
+
+        wynik = lista.pop(0)
+        while len(lista) > 0:
+            wynik = wynik * lista.pop(0)
+
+        return wynik
+
+    while len(str(n)) > 1:
+        i += 1
+        n = multiplication(int_to_list(n))
+
+    return i
 
 
 if __name__ == '__main__':
