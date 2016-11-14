bc45481663798651884ab5c04c6b53d618c22f2f
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2016-11-14 11:18:40 +0100

    Formatowanie wyciągnąłem do osobnej funkcji.

--- day02/homework/program1.py
+++ day02/homework/program1.py
@@ -2,8 +2,13 @@
 import day02.homework.kolo as p
 
 
+def pr(text, dlugosc=12) -> str:
+    wynik = str(text).rjust(dlugosc, ' ')
+    return wynik
+
+
 for i in range(21):
-    obwod = str(round(p.licz_obwod(i), 2)).rjust(12, ' ')
-    pole = str(round(p.licz_pole_kola(i), 2)).rjust(12, ' ')
-    promien = str(i).rjust(12, ' ')
+    obwod = pr(round(p.licz_obwod(i), 2))
+    pole = pr(round(p.licz_pole_kola(i), 2))
+    promien = pr(i)
     print('promień: ', promien, '; obwód: ', obwod,  '; pole: ', pole)
