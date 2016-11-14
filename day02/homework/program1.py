a020f3e7547933d0fb39154c46054dc7a5eef044
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-14 11:43:11 +0100

    Zmieniłem nazwę funkcji pr -> do_prawej

--- day02/homework/program1.py
+++ day02/homework/program1.py
@@ -2,14 +2,14 @@
 import day02.homework.kolo as p
 
 
-def pr(text, dlugosc=12) -> str:
+def do_prawej(text, dlugosc=12) -> str:
     wynik = str(text).rjust(dlugosc, ' ')
     return wynik
 
 print('Rozpoczynam wyliczenia:')
 
 for i in range(1, 21):
-    obwod = pr(p.licz_obwod(i))
-    pole = pr(p.licz_pole_kola(i))
-    promien = pr(i)
+    obwod = do_prawej(p.licz_obwod(i))
+    pole = do_prawej(p.licz_pole_kola(i))
+    promien = do_prawej(i)
     print('promień: ', promien, '; obwód: ', obwod,  '; pole: ', pole)
