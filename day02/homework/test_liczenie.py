03330bf00d9175b88e328a990080851f2c15c87e
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2016-11-14 10:33:45 +0100

    Test dla funkcji oblicz pole koła

--- day02/homework/test_liczenie.py
+++ day02/homework/test_liczenie.py
@@ -4,3 +4,7 @@ import day02.homework.kolo as fun
 
 def test_licz_obwod():
     assert 6.28 == pytest.approx(fun.licz_obwod(1))
+
+
+def test_licz_pole_kola():
+    assert 3.14 == pytest.approx(fun.licz_pole_kola(1))
