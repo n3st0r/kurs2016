37eea0a956d3b6555e53ea94b0c43df2eabd5558
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2016-11-14 10:35:47 +0100

    Funkcja liczące pole koła

--- day02/homework/kolo.py
+++ day02/homework/kolo.py
@@ -2,12 +2,24 @@
 Pi = 3.14
 
 
-def licz_obwod(r: int) -> int:
+def licz_obwod(r: float) -> float:
     """
     Funkcja liczy promień koła
     :param r: promień koła, niezbędny do wyliczenia obwodu
-    :return: int
+    :return: float
     """
 
     obwod = 2 * Pi * r
     return obwod
+
+
+def licz_pole_kola(r: float) -> float:
+    """
+    Funkcja liczy pole koła
+
+    :param r:
+    :return: float
+    """
+
+    pole = Pi * (r**2)
+    return pole
