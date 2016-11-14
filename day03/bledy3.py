bec25ff692a358483b942e05ce0def40f647aedf
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-14 18:31:19 +0100

    Obsługa różnych błędów, łącznie z Catch All.

---
+++ day03/bledy3.py
@@ -0,0 +1,20 @@
+import day02.homework.kolo as kolo
+
+
+liczba = input('Podaj liczbę: ')
+l = [1, 2]
+k = (1, 2)
+
+try:
+    liczba = int(liczba)
+    wynik = l[liczba]
+except ValueError:
+    print('Nie podałeś liczby!')
+except IndexError:
+    print('Lista nie posiada tylu elementów!')
+    print('LOG: ', e)
+except Exception as e:
+    print('LOG: ', e)
+else:
+    wynik = liczba / 10
+    print('Wynik: ', wynik)
