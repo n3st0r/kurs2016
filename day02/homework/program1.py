e3ffeb755c0a03b3f3aa61b1deb4f622cbe2c312
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2016-11-14 11:00:10 +0100

    Użyłem zaokrąglenia w drukowaniu wyników

--- day02/homework/program1.py
+++ day02/homework/program1.py
@@ -3,4 +3,6 @@ import day02.homework.kolo as p
 
 
 for i in range(21):
-    print('promień: ', i, '; obwód: ', p.licz_obwod(i),  '; pole: ', p.licz_pole_kola(i))
+    obwod = round(p.licz_obwod(i), 2)
+    pole = round(p.licz_pole_kola(i), 2)
+    print('promień: ', i, '; obwód: ', obwod,  '; pole: ', pole)
