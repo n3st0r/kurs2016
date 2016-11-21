355eb13253ff32a6e780e784ce358cfa02202a64
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-21 19:56:26 +0100

    Rozwiązanie zadania 2

---
+++ day05/zadanie2.py
@@ -0,0 +1,33 @@
+
+
+class Samochod:
+
+    def __init__(self, max, spalanie):
+        self.v_max = max
+        self.spalanie = spalanie
+        self.przebieg = 0
+        self.spalone_paliwo = 0
+
+
+    def przejedz(self, predkosc):
+        if predkosc > self.v_max:
+            print("BŁĄD")
+        else:
+            czas = 30
+            self.przebieg += predkosc * czas
+            self.spalone_paliwo = self.przebieg * self.spalanie
+
+    def poka_przebieg(self):
+        print(self.przebieg)
+
+    def __str__(self):
+        wynik = 'Maksymalna prędkość: %s, Przebieg samochodu: %s, Spalone %s'\
+                % (self.v_max, self.przebieg, self.spalone_paliwo)
+        return wynik
+
+if __name__ == '__main__':
+    bmw = Samochod(200, 7)
+    bmw.przejedz(150)
+    bmw.poka_przebieg()
+
+    print(bmw)
