4e280ad5ae239f9deb8fb4557076de98c2fb1780
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-21 17:14:41 +0100

    Ostatnie zmiany w kodzie dnia czwartego i start piątego.

--- day04/prostokat_do_pliku.py
+++ day04/prostokat_do_pliku.py
@@ -11,11 +11,21 @@ def podaj_boki():
     b = podaj_liczbe('Podaj długość boku B: ')
     return a, b
 
+
+def zapis_do_pliku(nazwa_pliku, tekst):
+    """
+        bok a: 1, bok b: 2, pole:2
+        """
+    zawartosc = open(nazwa_pliku, 'a')
+    zawartosc.write(tekst)
+    zawartosc.close()
+
+
 if __name__ == '__main__':
     print('Komunikat')
     boka = float()
     bokb = float()
-    message = str()
+
     try:
         boka, bokb = podaj_boki()
     except ValueError as e:
@@ -24,14 +34,9 @@ if __name__ == '__main__':
 
     pole = boka * bokb
     print('Pole prostokąta wynosi: ', pole)
+    message = 'nie'
     if boka == bokb:
-        message = 'Podany prostokąt jest kwadratem!'
-        print(message)
+        message = 'tak'
+        print('Podany prostokąt jest kwadratem!')
 
-    """
-    bok a: 1, bok b: 2, pole:2
-    """
-    plik = 'wyniki.txt'
-    zawartosc = open(plik, 'a')
-    zawartosc.write('bok a: %s, bok b: %s, pole: %s, %s\n' % (boka, str(bokb), str(pole), message))
-
+    wynik = 'bok a: %s, bok b: %s, pole: %s, kwadrat: %s\n' % (boka, bokb, pole, message)
