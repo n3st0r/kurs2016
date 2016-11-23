92d649febb8f3e098db6d7ff47caed57f9544ea9
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-23 18:36:54 +0100

    funkcja na obiekcie

---
+++ day06/klasy2.py
@@ -0,0 +1,36 @@
+
+class Zwierze:
+    def __init__(self):
+        self.wiek = 0
+        print('Zwierze - __init__')
+
+    def jedz(self, pokarm):
+        print('Zjadłem: ', pokarm)
+
+    def jaki_wiek(self):
+        print('Mam lat:', self.wiek)
+
+
+class KotDomowy(Zwierze):
+    def __init__(self):
+        super().__init__()
+        print('Kot domowy - __init__')
+
+    def machaj_ogonem(self):
+        print('macha ogonem')
+
+class Waz(Zwierze):
+    pass
+
+def nakarm(cos):
+    cos.jedz('pajęczyny')
+
+if __name__ == '__main__':
+    z1 = Zwierze()
+    nakarm(z1)
+    w1 = Zwierze()
+    nakarm(w1)
+
+    k1 = KotDomowy()
+    k1.machaj_ogonem()
+    k1.jedz('Mleko')
