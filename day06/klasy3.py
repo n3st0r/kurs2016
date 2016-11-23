ecf577fcf798618766836dca76ee37fcbbcdb7f1
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-23 19:10:49 +0100

    Metody statyczne c.d.

--- day06/klasy3.py
+++ day06/klasy3.py
@@ -3,6 +3,7 @@ import datetime
 class Zwierze:
     def __init__(self):
         self.wiek = 0
+        self.chip = ''
         print('Zwierze - __init__')
 
     def jedz(self, pokarm):
@@ -11,12 +12,20 @@ class Zwierze:
     def jaki_wiek(self):
         print('Mam lat:', self.wiek)
 
+    def nadaj_chip(self):
+        self.chip = self.generuj_chip()
+
     @staticmethod
     def generuj_chip():
         chip = datetime.datetime.now()
         return str(chip)
 
 if __name__ == '__main__':
-    # z1 = Zwierze()
-    chipek = Zwierze.generuj_chip()
-    print(chipek)
+    z1 = Zwierze()
+    chip = Zwierze.generuj_chip()
+    print(chip)
+    chip = z1.generuj_chip()
+    print(chip)
+    z2 = Zwierze
+    chip = z2.generuj_chip()
+    print(chip)
