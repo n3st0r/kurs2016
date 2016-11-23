4c634d9dcee2a19ba23f05b7b3f08cebcea02fa4
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-23 19:55:29 +0100

    Try-except-else-finally.

--- day06/bledy4.py
+++ day06/bledy4.py
@@ -1,8 +1,16 @@
 
 try:
-    f = open('plik, którego nie ma')
+    f = open('a')
+    c = f.read()
+    liczba = int(c)
 except FileNotFoundError:
     print('Pliku nie ma')
+except ValueError:
+    print('Błędy w pliku')
+    raise
 else:
     c= f.read()
     print(c)
+finally:
+    f.close()
+    print('finally')
