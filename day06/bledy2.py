dbd7c7f4145ecd38959698af620a17f211c9bea4
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-23 19:43:25 +0100

    Dalsza customizacja obsługi błędu.

--- day06/bledy2.py
+++ day06/bledy2.py
@@ -1,19 +1,27 @@
 class NiedobraKarma(Exception):
-    pass
+    def __init__(self, imie_kota):
+        self.imie_kota = imie_kota
+
+    def __str__(self):
+        return '%s: Nie nawidzę jedzenia z puszki!' % self.imie_kota
 
 
 class KotDomowy:
+    def __init__(self, imie):
+        self.imie = imie
+
     def jedz(self,pokarm):
         if pokarm != 'mysz':
-            raise NiedobraKarma('Jadam tylko myszy')
-        print('mniam mniam')
+            raise NiedobraKarma(self.imie)
+        print(self.imie, ': mniam mniam')
 
 if __name__ == '__main__':
-    k1 = KotDomowy()
+    k1 = KotDomowy('filemon')
     k1.jedz('mysz')
 
-    k2 = KotDomowy()
+    k2 = KotDomowy('bonifacy')
     try:
         k2.jedz('Whiskas')
-    except NiedobraKarma:
-        print('Nie zjadł whiskas, nic dziwnego')
+    except NiedobraKarma as ex:
+        print(ex)
+        # print('Nie zjadł whiskas, nic dziwnego')
