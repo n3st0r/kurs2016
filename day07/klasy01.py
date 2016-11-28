91343ccad10b6daf058433ef2c8368612c3fd43a
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2016-11-28 18:07:33 +0100

    Klasy 01 - c.d.

---
+++ day07/klasy01.py
@@ -0,0 +1,16 @@
+
+class Kotek:
+    def __init__(self):
+        self.wiek = 0
+
+    def kicikici(self):
+        print('miau')
+
+    def ile_lat(self):
+        print(self.wiek)
+
+if __name__ == '__main__':
+    k1 = Kotek()
+    k1.kicikici()
+    k1.ile_lat()
+
